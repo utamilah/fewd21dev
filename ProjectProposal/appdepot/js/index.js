a69ebab2240c4fdbc8aeb6ee5954abffeaf71cfe
utamilah <bakarr@Admins-MacBook-Pro-5.local>
2016-03-21 15:41:43 -0400

    App Depot Progress 3/21

--- ProjectProposal/appdepot/js/index.js
+++ ProjectProposal/appdepot/js/index.js
@@ -1,5 +1,47 @@
 $(document).ready(function(e){
 	//Examples of how to assign the Colorbox event to elements			
 	$(".inline").colorbox({inline:true});
-			
+
+	$('.appContainer').mixItUp();
+
+	$(".nav").sticky({topSpacing:0});
+
+	$.scrollIt();
+	
 });
+
+var waypoints = $('.stats').waypoint({
+  handler: function(direction) {
+    console.log('reached')
+
+   var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+	$('#number1').animateNumber(
+	  {
+	    number: 1600000,
+	    numberStep: comma_separator_number_step
+	  }
+	);
+
+	$('#number2').animateNumber(
+	  {
+	    number: 1500000,
+	    numberStep: comma_separator_number_step
+	  }
+	);
+
+	$('#number3').animateNumber(
+	  {
+	    number: 400000,
+	    numberStep: comma_separator_number_step
+	  }
+	);
+
+	$('#number4').animateNumber(
+	  {
+	    number: 340000,
+	    numberStep: comma_separator_number_step
+	  }
+	);
+
+  }
+})
